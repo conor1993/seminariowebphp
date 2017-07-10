91f454dad5cabaec060aadc6036922710c46bbbb
connor1993 <manuellugo1993@gmail.com>
2017-07-10 11:26:21 -0600

    css login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+ <link href="/css/app.css" rel="stylesheet">
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
