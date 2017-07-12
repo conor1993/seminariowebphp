be637ff05b90709e140a1874da0218804d17cde8
connor1993 <manuellugo1993@gmail.com>
2017-07-12 08:56:41 -0600

    correccion de errrores

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+   
 
     /**
      * Create a new controller instance.
@@ -36,6 +36,7 @@ class RegisterController extends Controller
      */
     public function __construct()
     {
+        
         $this->middleware('guest');
     }
 
